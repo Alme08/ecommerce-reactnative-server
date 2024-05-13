1608c487716ab5333cfc444aa779f115650df2cc
Jose <93064976+Alme08@users.noreply.github.com>
2024-05-12 22:11:43 -0400

    add clg

--- utils/features.js
+++ utils/features.js
@@ -6,3 +6,5 @@ export const getDataUri = file => {
 	const extName = path.extname(file.fileName).toString();
 	return parser.format(extName, file.buffer);
 };
+
+// {"assetId": null, "base64": null, "duration": null, "exif": null, "fileName": "6892e94a-522f-48b7-bb29-02df425d5123.jpeg", "fileSize": 4209745, "height": 2160, "mimeType": "image/jpeg", "rotation": null, "type": "image", "uri": "file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540anonymous%252Fclient-a9f4794d-96cf-4133-b29e-c75bffa7e5fc/ImagePicker/6892e94a-522f-48b7-bb29-02df425d5123.jpeg", "width": 2880}
