fe456a51b4d3460b2aa091acc12d6baa860e52e5
Jose <93064976+Alme08@users.noreply.github.com>
2024-05-14 22:04:01 -0400

    update

--- utils/features.js
+++ utils/features.js
@@ -5,6 +5,5 @@ export const getDataUri = file => {
 	// const parser = new DataURIParser();
 	// const extName = path.extname(file.originalname).toString();
 	// return parser.format(extName, file.buffer);
-	console.log(file, file.mimeType);
 	return `data:${file.mimeType};base64,${file.base64}`;
 };
