34a66e1ce4b384597a7a04c1d47166b604646d46
Jose <93064976+Alme08@users.noreply.github.com>
2024-05-25 21:50:35 -0400

    update

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -2,4 +2,4 @@ import multer from 'multer';
 
 const storage = multer.memoryStorage();
 
-export const singleUpload = multer({ storage }).single('file' || 'body');
+export const singleUpload = multer({ storage }).single('file');
