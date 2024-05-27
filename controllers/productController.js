3bf661fa6e5abd457a08c67cc26092e335d0ddad
Jose <93064976+Alme08@users.noreply.github.com>
2024-05-26 22:41:45 -0400

    update

--- controllers/productController.js
+++ controllers/productController.js
@@ -96,7 +96,6 @@ export const getSingleProductController = async (req, res) => {
 // CREATE PRODUCT
 export const createProductController = async (req, res) => {
 	try {
-		console.log(req.body, req.file, req);
 		const { name, description, price, stock, category } = req.body;
 		//validation
 		if (!name || !description || !price || !stock) {
@@ -189,7 +188,7 @@ export const updateProductImageController = async (req, res) => {
 	try {
 		//find product
 		const product = await productModel.findById(req.params.id);
-		console.log(req.params, req.file);
+		console.log(req.file);
 		//validation
 		if (!product) {
 			console.log('product not found');
