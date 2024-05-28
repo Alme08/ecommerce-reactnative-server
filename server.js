e6fbde76da1a89527c1fac37f2cb4d1a49d27b74
Jose <93064976+Alme08@users.noreply.github.com>
2024-05-28 19:33:07 -0400

    update

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ const app = express();
 app.use(helmet());
 app.use(mongoSanitize());
 app.use(morgan('dev'));
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(cors());
 app.use(cookieParser());
-app.use(bodyParser.json({ limit: '50mb' }));
 // var bodyParser = require('body-parser');
 // app.use(bodyParser.json({ limit: '50mb' }));
 // app.use(
