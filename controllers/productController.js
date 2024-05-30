842cc818a73c6fbcf3182f5b44ad65eae5af1978
Jose <93064976+Alme08@users.noreply.github.com>
2024-05-29 21:32:35 -0400

    update

--- controllers/productController.js
+++ controllers/productController.js
@@ -96,7 +96,6 @@ export const getSingleProductController = async (req, res) => {
 // CREATE PRODUCT
 export const createProductController = async (req, res) => {
 	try {
-		console.log(req.file, req.body);
 		const { name, description, price, stock, category } = req.body;
 		//validation
 		if (!name || !description || !price || !stock) {
